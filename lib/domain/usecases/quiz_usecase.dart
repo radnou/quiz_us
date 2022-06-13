1adc3623a37c80fb4f554f55da50549bbdaf13e2
Radnoumane Mossabely <radnou.m@gmail.com>
2022-06-13 02:58:23 +0200

    :seedling: Set up use case method and fix widget test

--- lib/domain/usecases/quiz_usecase.dart
+++ lib/domain/usecases/quiz_usecase.dart
@@ -1,4 +1,12 @@
+import 'dart:math';
 
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:quiz_us/data/repositories/quiz_repository_impl.dart';
+import 'package:quiz_us/domain/entities/question.dart';
+import 'package:quiz_us/domain/repositories/quiz_repository.dart';
+
+final quizUseCaseProvider = Provider<QuizUseCase>(
+    (ref) => QuizUseCase(ref.read(QuizRepositoryProvider)));
 
 class QuizUseCase {
   final QuizRepository _repository;
@@ -7,6 +15,7 @@ class QuizUseCase {
   QuizUseCase(this._repository);
 
   Future<List<Question>> getQuestions() {
-    return _repository;
+    return _repository.getQuestions(
+        numQuestions: 5, categoryId: Random().nextInt(24) + 9);
   }
 }
