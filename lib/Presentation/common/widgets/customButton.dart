1b7d170c24e3b042009ffb57005eeab43610dab9
Radnoumane Mossabely <radnou.m@gmail.com>
2022-06-13 04:28:48 +0200

    :seedling: Classes Error et Custom button

---
+++ lib/Presentation/common/widgets/customButton.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String title;
+  final VoidCallback onTap;
+  const CustomButton({Key? key, required this.title, required this.onTap}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: const EdgeInsets.all(20.0),
+        height: 50.0,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Color(0xffE6812F),
+          borderRadius: BorderRadius.circular(10.0)
+         ),
+         alignment: Alignment.center,
+         child: Text(
+          title,
+          style: const TextStyle(
+            fontSize: 18.0,
+            color: Colors.white,
+            fontWeight: FontWeight.w600
+          ),
+         ),
+      ),
+    );
+  }
+}
